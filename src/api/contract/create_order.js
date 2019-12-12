fa1cc3f10d03b7368c97e23d98a6df28793fa174
ali@purple7.com <ali@purple7.com>
2019-12-12 15:33:59 +0000

    minor formatting update

--- src/api/contract/create_order.js
+++ src/api/contract/create_order.js
@@ -1,63 +1,63 @@
-const {generatePseudoRandomSalt} = require('@0x/order-utils')
+const { generatePseudoRandomSalt } = require('@0x/order-utils');
 const BigNumber = require('bignumber.js');
 const sw = require('starkware_crypto');
 
 module.exports = (efx, symbol, amount, price, validFor, fee_rate = 0.0025, vault_id_buy, vault_id_sell) => {
-  // symbols are always 3 letters
-  const baseSymbol = symbol.substr(0, symbol.length - 3)
-  const quoteSymbol = symbol.substr(-3)
+	// symbols are always 3 letters
+	const baseSymbol = symbol.substr(0, symbol.length - 3);
+	const quoteSymbol = symbol.substr(-3);
 
-  const buySymbol = amount > 0 ? baseSymbol : quoteSymbol
-  const sellSymbol = amount > 0 ? quoteSymbol : baseSymbol
+	const buySymbol = amount > 0 ? baseSymbol : quoteSymbol;
+	const sellSymbol = amount > 0 ? quoteSymbol : baseSymbol;
 
-  const sellCurrency = efx.config['0x'].tokenRegistry[sellSymbol]
-  const buyCurrency = efx.config['0x'].tokenRegistry[buySymbol]
+	const sellCurrency = efx.config['0x'].tokenRegistry[sellSymbol];
+	const buyCurrency = efx.config['0x'].tokenRegistry[buySymbol];
 
-  let buyAmount, sellAmount
+	let buyAmount, sellAmount;
 
-  if (amount > 0) {
-    buyAmount = (new BigNumber(10))
-      .pow(buyCurrency.decimals)
-      .times(amount)
-      .times(1 + (buyCurrency.settleSpread || 0))
-      .times(1 - fee_rate)
-      .integerValue(BigNumber.ROUND_FLOOR)
+	if (amount > 0) {
+		buyAmount = new BigNumber(10)
+			.pow(buyCurrency.decimals)
+			.times(amount)
+			.times(1 + (buyCurrency.settleSpread || 0))
+			.times(1 - fee_rate)
+			.integerValue(BigNumber.ROUND_FLOOR);
 
-    sellAmount = (new BigNumber(10))
-      .pow(sellCurrency.decimals)
-      .times(amount)
-      .times(price)
-      .times(1 + (sellCurrency.settleSpread || 0))
-      .integerValue(BigNumber.ROUND_FLOOR)
+		sellAmount = new BigNumber(10)
+			.pow(sellCurrency.decimals)
+			.times(amount)
+			.times(price)
+			.times(1 + (sellCurrency.settleSpread || 0))
+			.integerValue(BigNumber.ROUND_FLOOR);
 
-    // console.log( "Buying " + amount + ' ' + buySymbol + " for: " + price + ' ' + sellSymbol )
-  }
+		// console.log( "Buying " + amount + ' ' + buySymbol + " for: " + price + ' ' + sellSymbol )
+	}
 
-  if (amount < 0) {
-    buyAmount = (new BigNumber(10))
-      .pow(buyCurrency.decimals)
-      .times(amount)
-      .times(price)
-      .abs()
-      .times(1 + (buyCurrency.settleSpread || 0))
-      .times(1 - fee_rate)
-      .integerValue(BigNumber.ROUND_FLOOR)
+	if (amount < 0) {
+		buyAmount = new BigNumber(10)
+			.pow(buyCurrency.decimals)
+			.times(amount)
+			.times(price)
+			.abs()
+			.times(1 + (buyCurrency.settleSpread || 0))
+			.times(1 - fee_rate)
+			.integerValue(BigNumber.ROUND_FLOOR);
 
-    sellAmount = (new BigNumber(10))
-      .pow(sellCurrency.decimals)
-      .times(amount)
-      .abs()
-      .times(1 + (sellCurrency.settleSpread || 0))
-      .integerValue(BigNumber.ROUND_FLOOR)
+		sellAmount = new BigNumber(10)
+			.pow(sellCurrency.decimals)
+			.times(amount)
+			.abs()
+			.times(1 + (sellCurrency.settleSpread || 0))
+			.integerValue(BigNumber.ROUND_FLOOR);
 
-    // console.log( "Selling " + Math.abs(amount) + ' ' + sellSymbol + " for: " + price + ' ' + buySymbol )
-  }
+		// console.log( "Selling " + Math.abs(amount) + ' ' + sellSymbol + " for: " + price + ' ' + buySymbol )
+	}
 
-  let expiration
-  expiration = Math.round((new Date()).getTime() / 1000)
-  expiration += validFor || config.defaultExpiry
+	let expiration;
+	expiration = Math.round(new Date().getTime() / 1000);
+	expiration += validFor || config.defaultExpiry;
 
-  var starkOrder = {
+	var starkOrder = {
 		vault_id_sell: vault_id_sell,
 		vault_id_buy: vault_id_buy,
 		amount_sell: amount_sell,
@@ -65,10 +65,10 @@ module.exports = (efx, symbol, amount, price, validFor, fee_rate = 0.0025, vault
 		token_sell: sellCurrency.tokenId,
 		token_buy: buyCurrency.tokenId,
 		nonce: generatePseudoRandomSalt(),
-		expiration_timestamp: new BigNumber(expiration)
-  };
-  
-	let starkMessage = ''
+		expiration_timestamp: new BigNumber(expiration),
+	};
+
+	let starkMessage = '';
 	try {
 		//Create stark message for order
 		starkMessage = sw.get_limit_order_msg(
@@ -80,13 +80,12 @@ module.exports = (efx, symbol, amount, price, validFor, fee_rate = 0.0025, vault
 			buyCurrency.tokenId, // token_buy (hex str with 0x prefix < prime)
 			starkOrder.nonce, // nonce (uint31)
 			starkOrder.expiration_timestamp // expiration_timestamp (uint22)
-    );
-    }
-    catch(e)  {
-      throw('unable to create stark order message')
-    }
-  return {
-    starkOrder: starkOrder,
-    starkMessage: starkMessage
-  }
-}
+		);
+	} catch (e) {
+		throw 'unable to create stark order message';
+	}
+	return {
+		starkOrder: starkOrder,
+		starkMessage: starkMessage,
+	};
+};
--- src/api/stark/submit_order.js
+++ src/api/stark/submit_order.js
@@ -23,23 +23,23 @@ module.exports = async (
 
 	//TODO:
 	//User Specific Parameters to be retrieved via getUserConfig
-	var private_key = '3c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc'
+	var private_key = '3c1e9550e66958296d11b60f8e8e7a7ad990d07fa65d5f7652c4a6c87d4e3cc';
 	var key_pair = sw.ec.keyFromPrivate(private_key, 'hex');
-	var public_key = sw.ec.keyFromPublic(key_pair.getPublic(true, 'hex'), 'hex')
-	const starkKey = public_key.pub.getX().toString()
-	const starkKeyPair = key_pair
+	var public_key = sw.ec.keyFromPublic(key_pair.getPublic(true, 'hex'), 'hex');
+	const starkKey = public_key.pub.getX().toString();
+	const starkKeyPair = key_pair;
 
-	const {starkOrder,starkMessage} = efx.stark.createOrder(
-		symbol, 
-		amount, 
-		price, 
-		validFor, 
+	const { starkOrder, starkMessage } = efx.stark.createOrder(
+		symbol,
+		amount,
+		price,
+		validFor,
 		fee_rate,
 		vault_id_buy,
 		vault_id_sell
-		);
+	);
 
-	const starkSignature = efx.stark.signOrder(starkKeyPair,starkMessage);
+	const starkSignature = efx.stark.signOrder(starkKeyPair, starkMessage);
 
 	if (!fee_rate) {
 		if (!dynamicFeeRate) {
