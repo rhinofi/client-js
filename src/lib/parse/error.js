325e9216feda6f0ef4cfa538cc49aa4bc5af0e9a
ali@purple7.com <ali@purple7.com>
2019-12-21 11:55:05 +0000

    auto add MISSING or INVALID

--- src/lib/parse/error.js
+++ src/lib/parse/error.js
@@ -1,19 +1,33 @@
 const reasons = require('../error/reasons')
 
-module.exports = (errorArray) => {
-  // get the last word from the error
-  const message = errorArray[2].split(' ').pop()
+/*
+  1. Can return multiple errors (need to modify)
+  2. Can handle error functions
+*/
+module.exports = async (args) => {
+  let efx = args.splice(0, 1)
+  let error
 
-  let error = {
-    code: errorArray[1],
-    message: errorArray[2]
-  }
-
-  for (let message in reasons) {
-    if (error.message == message) {
-      error.reason = reasons[message].trim()
+  for (let objVal of args) {
+    let { key, value, conditions } = objVal
+    for (let cond of conditions) {
+      if (typeof cond !== 'string') {
+        error = await cond(value)
+        if (error) error = `${key} - ` + error
+        else error = null
+      }
+      switch (cond) {
+        case 'is_empty':
+          if (!value) error = `${key} - ERR_EMPTY`
+          break
+        case 'is_invalid':
+          if (typeof value !== 'string' && value <= 0) error = `${key} - ERR_INVALID`
+          break
+      }
+      if (error) break
     }
+    if (error) break
   }
 
-  return {error}
+  return { error }
 }
