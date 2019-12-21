e5625f5ef45ae0f823b6b24bd593d536e94e113f
ali@purple7.com <ali@purple7.com>
2019-12-21 11:57:44 +0000

    added price, stark key, stark key pair to assertions

--- src/lib/validators/validateAssertions.js
+++ src/lib/validators/validateAssertions.js
@@ -4,24 +4,29 @@ validateToken = require('./validateToken')
 validateNonce = require('./validateNonce')
 validateSignature = require('./validateSignature')
 validateAmount = require('./validateAmount')
+validatePrice = require('./validatePrice')
+validateStarkKey = require('./validateStarkKey')
+validateStarkKeyPair = require('./validateStarkKeyPair')
 
 module.exports = parameters => {
-	var keys = Object.keys(parameters)
-	for (let key of keys) {
-    if ((result = assertionErrors(key, parameters[key], parameters.efx))) 
-      return result;
-	}
-	return false;
-};
+  var keys = Object.keys(parameters)
+  for (let key of keys) {
+    if ((result = assertionErrors(key, parameters[key], parameters.efx))) { return result }
+  }
+  return false
+}
 
 assertionErrors = (param, value, efx) => {
-	const validators = {
-		orderId: validateOrderId(value),
-		symbol: validateSymbol(efx, value),
-		token: validateToken(efx, value),
-		nonce: validateNonce(value),
-		signature: validateSignature(value),
-		amount: validateAmount(value),
-	}
-	return validators[param]
+  const validators = {
+    orderId: validateOrderId(value),
+    symbol: validateSymbol(efx, value),
+    token: validateToken(efx, value),
+    nonce: validateNonce(value),
+    signature: validateSignature(value),
+    amount: validateAmount(value),
+    price: validatePrice(value),
+    starkKey: validateStarkKey(value),
+    starkKeyPair: validateStarkKeyPair(value)
+  }
+  return validators[param]
 }
