fe63dde6cb1caf6ac1237c3c9a94f1d901b7ff4c
ali@purple7.com <ali@purple7.com>
2019-12-21 12:00:17 +0000

    enhanced validate symbol validation method

--- src/lib/validators/validateSymbol.js
+++ src/lib/validators/validateSymbol.js
@@ -7,4 +7,14 @@ module.exports = (efx, symbol) => {
       reason: errorReasons.ERR_INVALID_SYMBOL || 'ERR_INVALID_SYMBOL'
     }
   }
+  if (symbol) {
+    let from = symbol.length == 6 && symbol.substr(0, symbol.length - 3)
+    let to = symbol.length == 6 && symbol.substr(-3)
+    if (!efx.config.tokenRegistry[from] || !efx.config.tokenRegistry[to]) {
+      return {
+        error: 'ERR_INVALID_SYMBOL',
+        reason: errorReasons.ERR_INVALID_SYMBOL || 'ERR_INVALID_SYMBOL'
+      }
+    }
+  }
 }
