89029b901a632566976413b97d21196305e702ee
AliMeer <ali@purple7.com>
2020-01-14 00:38:29 +0000

    getOrder and getOrders test coverage

--- src/api/getOrders.test.js
+++ src/api/getOrders.test.js
@@ -8,47 +8,37 @@ const mockGetUserConf = require('./test/fixtures/getUserConf')
 let dvf
 
 describe('getOrder', () => {
-
   beforeAll(async () => {
     mockGetConf()
     mockGetUserConf()
     dvf = await instance()
   })
 
-// TODO:
-/*
+  it('Fetching orders from the API', async done => {
+    const apiResponse = { id: '408231' }
 
-describe('/getOrder', () => {
-  it('GetOrder request to DVF pub api....', async () => {
-    const orderId = 1
-    const apiResponse = [[1234]]
-    const nonce = Date.now() / 1000 + 30 + ''
-    const signature = await efx.sign(nonce.toString(16))
-
-    nock('https://staging-api.deversifi.com/')
-      .post('/v1/trading/r/getOrder', async body => {
-        assert.equal(body.orderId, orderId)
-        assert.equal(body.nonce, nonce)
-        assert.equal(body.signature, signature)
-        return true
+    nock('https://app.stg.deversifi.com/')
+      .post('/v1/trading/r/openOrders', body => {
+        return (
+          _.isMatch(body, {
+            symbol: 'ETH:USDT'
+          }) &&
+          body.nonce &&
+          body.signature
+        )
       })
       .reply(200, apiResponse)
-    const response = await efx.getOrder(orderId, nonce, signature)
-    console.log('got result =>', response)
+
+    const response = await dvf.getOrders('ETH:USDT')
+    expect(response.id).toEqual(apiResponse.id)
+
+    done()
   })
 
-  it('GetOrder checks for orderId....', async () => {
-    nock('https://staging-api.deversifi.com/')
-      .post('/v1/trading/r/getOrder', async body => {
-        assert.equal(body.error, 'ERR_INVALID_ORDER_ID')
-        return true
-      })
-      .reply(200)
-    const response = await efx.getOrder('', '', '')
-    console.log('got result =>', response)
+  it('GetOrder checks for orderId....', async done => {
+    const orders = await dvf.getOrders(null)
+    expect(orders.error).toEqual('ERR_INVALID_SYMBOL')
+
+    done()
   })
-})
-
-*/
-
 })
