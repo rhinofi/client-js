9417f0cd8e61981e8f7ab8b5e8033a030db6f49e
AliMeer <ali@purple7.com>
2020-01-17 17:59:01 +0000

    using int for vault ids in mock config

--- src/api/getUserConfig.test.js
+++ src/api/getUserConfig.test.js
@@ -14,12 +14,12 @@ describe('getUserConfig', () => {
   it('Returns the user config recieved from the API', async done => {
     const apiResponse = {
       DVF: {
-        depositExpiry: 720,
-        depositNonce: 1,
+        exchangeSymbols: ['tETHUSD', 'tZRXUSD', 'tZRXETH'],
         exchangeAddress: '0xBd25cD867C304F079E696CBE44D958f3d3B683ba',
         starkExContractAddress: '0xF3731d0cdC9834f6F32104580bD226EF1bc1A9F9',
-        exchangeSymbols: ['tETHUSD', 'tZRXUSD', 'tZRXETH'],
-        tempStarkVaultId: 1
+        tempStarkVaultId: 1,
+        depositExpiry: 720,
+        depositNonce: 1
       },
       tokenRegistry: {
         ETH: {
@@ -28,25 +28,25 @@ describe('getUserConfig', () => {
           starkTokenId: '0x1',
           starkVaultId: 1251434240
         },
-        USD: {
+        USDT: {
           decimals: 6,
+          tokenAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
           minOrderSize: 25,
           settleSpread: -0.026,
           starkTokenId: '0x2',
-          tokenAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
           starkVaultId: 9876512
         },
         ZRX: {
-          decimals: 18,
+          decimals: 16,
+          tokenAddress: '0xe41d2489571d322189246dafa5ebde1f4699f498',
           minOrderSize: 40,
           starkTokenId:
             '22e6d888f32dea3c6e8ba64609a314eebbe1eb704e9e9febe368b0bacb21efe',
-          tokenAddress: '0xe41d2489571d322189246dafa5ebde1f4699f498',
-          starkVaultId: 12345
+          starkVaultId: 1771994328
         }
       },
-      spareStarkVaultId: 1599136128,
-      ethAddress: '0x41c3df418b1a9dc20b4ad1da1a740d16519fba4d'
+      spareStarkVaultId: 2090569095,
+      ethAddress: '0xf858c2f2ac6b96df8c801bce90a3124a52d1915a'
     }
 
     nock(dvf.config.api)
