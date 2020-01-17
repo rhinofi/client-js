58576d7fb1bea7fb3440134155ee7f2e7c5e5dad
AliMeer <ali@purple7.com>
2020-01-17 10:48:57 +0000

    config, userConfig updated

--- src/api/getConfig.test.js
+++ src/api/getConfig.test.js
@@ -16,12 +16,12 @@ describe('getConfig', () => {
   it('Returns the config recieved from the API', async done => {
     const apiResponse = {
       DVF: {
-        exchangeSymbols: ['tETHUSD', 'tZRXUSD', 'tZRXETH'],
-        exchangeAddress: '0x1',
-        starkExContractAddress: '0xF3731d0cdC9834f6F32104580bD226EF1bc1A9F9',
-        tempStarkVaultId: 1,
         depositExpiry: 720,
-        depositNonce: 1
+        depositNonce: 1,
+        exchangeAddress: '0xBd25cD867C304F079E696CBE44D958f3d3B683ba',
+        starkExContractAddress: '0xF3731d0cdC9834f6F32104580bD226EF1bc1A9F9',
+        exchangeSymbols: ['tETHUSD', 'tZRXUSD', 'tZRXETH'],
+        tempStarkVaultId: 1
       },
       tokenRegistry: {
         ETH: {
@@ -31,19 +31,20 @@ describe('getConfig', () => {
         },
         USD: {
           decimals: 6,
-          tokenAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7',
           minOrderSize: 25,
           settleSpread: -0.026,
-          starkTokenId: '0x2'
+          starkTokenId: '0x2',
+          tokenAddress: '0xdac17f958d2ee523a2206206994597c13d831ec7'
         },
         ZRX: {
           decimals: 18,
-          tokenAddress: '0xe41d2489571d322189246dafa5ebde1f4699f498',
           minOrderSize: 40,
           starkTokenId:
-            '0x22e6d888f32dea3c6e8ba64609a314eebbe1eb704e9e9febe368b0bacb21efe'
+            '22e6d888f32dea3c6e8ba64609a314eebbe1eb704e9e9febe368b0bacb21efe',
+          tokenAddress: '0xe41d2489571d322189246dafa5ebde1f4699f498'
         }
-      }
+      },
+      spareStarkVaultId: 866076352
     }
 
     nock('https://app.stg.deversifi.com/')
