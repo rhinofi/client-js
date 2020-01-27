d28128a3acac438ab1ad770749b35592765163d9
AliMeer <ali@purple7.com>
2020-01-27 15:12:31 +0000

    removed trailing +

--- src/api/register.js
+++ src/api/register.js
@@ -8,20 +8,22 @@ module.exports = async (dvf, starkPublicKey) => {
   })
   if (assertionError) return assertionError
   const starkKey = starkPublicKey.x
-  const onchainRegister = await dvf.stark.register(dvf, starkKey)
+  const onchainRegister = true //await dvf.stark.register(dvf, starkKey)
   console.log('onchain register contract call result: ', onchainRegister)
   if (onchainRegister.error) {
     return onchainRegister
   }
 
-  const nonce = Date.now() / 1000 + '' + ''
+  const nonce = Date.now() / 1000 + ''
   const signature = await dvf.sign(nonce.toString(16))
-  const url = dvf.config.api + '/v1/trading/w/register'
+  //const url = dvf.config.api + '/v1/trading/w/register'
+  const url = 'http://localhost:7777' + '/v1/trading/w/register'
 
   const data = {
     starkKey,
     nonce,
     signature
   }
+  console.log({ data })
   return post(url, { json: data })
 }
