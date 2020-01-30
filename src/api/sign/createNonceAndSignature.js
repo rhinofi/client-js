2425000b1c2422daee516eceee9badae09ef6cd2
AliMeer <ali@purple7.com>
2020-01-30 19:18:43 +0000

    updating v#

--- src/api/sign/createNonceAndSignature.js
+++ src/api/sign/createNonceAndSignature.js
@@ -9,15 +9,7 @@
 module.exports = (dvf, nonce, signature) => {
   if (!(nonce && signature)) {
     nonce = Date.now() / 1000 + ''
-    if (dvf.web3.currentProvider.isMetaMask) {
-      signature = dvf.web3.eth.personal.sign(
-        nonce.toString(16),
-        dvf.get('account')
-      )
-    } else {
-      signature = dvf.web3.eth.sign(nonce.toString(16), dvf.get('account'))
-    }
-    //signature = await dvf.sign(nonce.toString(16))
+    signature = dvf.sign(nonce.toString(16))
   }
   return { nonce, signature }
 }
