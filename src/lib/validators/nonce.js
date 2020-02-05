249639983826f81ed0e765345fa877a70948b4de
Mateja Trifunovski <mtrifunovski@gmail.com>
2020-02-05 13:47:18 +0100

    Missing "new" keyword when throwing nonce errors

--- src/lib/validators/nonce.js
+++ src/lib/validators/nonce.js
@@ -4,10 +4,10 @@ module.exports = (dvf, nonce) => {
   if (typeof nonce === 'string') nonce = +nonce
 
   if (!nonce || isNaN(nonce)) {
-    throw DVFError('ERR_INVALID_NONCE')
+    throw new DVFError('ERR_INVALID_NONCE')
   }
   
   if ((Date.now() / 1000) - nonce > 1000) {
-    throw DVFError('NONCE_IS_TOO_OLD')
+    throw new DVFError('NONCE_IS_TOO_OLD')
   }
 }
