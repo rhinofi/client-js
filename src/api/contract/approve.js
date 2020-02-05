431ebd668c2ec895d1153ad0ae3825f0d74bdd10
hems <hems.inlet@gmail.com>
2020-02-05 10:08:29 +0000

    reviews and implements PR 32

--- src/api/contract/approve.js
+++ src/api/contract/approve.js
@@ -1,39 +1,31 @@
-const reasons = require('../../lib/dvf/DVFError')
+const DVFError = require('../../lib/dvf/DVFError')
 /**
  * Approves a token for locking
  *
  */
-module.exports = async (efx, token) => {
-  const currency = efx.token.getTokenInfo(token)
+module.exports = async (dvf, token) => {
+  const currency = dvf.token.getTokenInfo(token)
 
   const amount = (2 ** 256 - 1).toString(16)
 
   const args = [
-    currency.wrapperAddress, // address _spender
+    dvf.config.DVF.starkExContractAddress, // address _spender
     amount // uint amount
   ]
 
   // TODO: review error format
-  if (token === 'USD' && (await efx.contract.isApproved(token)) !== 0) {
-    return {
-      error: 'ERR_TRADING_ETHFX_CANT_APPROVE_USDT_TWICE',
-      reason: reasons.ERR_TRADING_ETHFX_CANT_APPROVE_USDT_TWICE.trim(),
-      originalError: e
-    }
+  if (token === 'USD' && (await dvf.contract.isApproved(token)) !== 0) {
+    throw new DVFError('ERR_TRADING_ETHFX_CANT_APPROVE_USDT_TWICE')
   }
 
   if (token === 'ETH') {
-    return {
-      error: 'ERR_TRADING_ETHFX_APPROVE_ETH_NOT_REQUIRED',
-      reason: reasons.ERR_TRADING_ETHFX_APPROVE_ETH_NOT_REQUIRED.trim(),
-      originalError: e
-    }
+    throw new DVFError('ERR_TRADING_ETHFX_APPROVE_ETH_NOT_REQUIRED')
   }
 
   const action = 'approve'
 
-  return efx.eth.send(
-    efx.contract.abi.token,
+  return dvf.eth.send(
+    dvf.contract.abi.token,
     currency.tokenAddress,
     action,
     args
