48ec2a7b229431e29e755690b9c3cce542a3282c
hems <hems.inlet@gmail.com>
2020-02-05 07:52:39 +0000

    implements basic auth and tests on cancelOrder

--- src/api/cancelOrder.test.js
+++ src/api/cancelOrder.test.js
@@ -11,17 +11,28 @@ describe('cancelOrder', () => {
     dvf = await instance()
   })
 
-  it('Posts to cancel order API and gets response', async done => {
+  it('Posts to cancel order API and gets response', async () => {
     const orderId = '1'
-    const apiResponse = { cancelOrder: 'success' }
+    const apiResponse = {cancelOrder: 'success'}
+    
+    const payloadValidator = jest.fn((body) => {
+      expect(body.orderId).toBe(orderId)
+      expect(typeof body.orderId).toBe('string')
+
+      return true
+    })
 
     nock(dvf.config.api)
-      .post('/v1/trading/w/cancelOrder', {
-        orderId: orderId
-      })
+      .post('/v1/trading/w/cancelOrder', payloadValidator)
       .reply(200, apiResponse)
+
     const response = await dvf.cancelOrder(orderId)
+    
+    expect(payloadValidator).toBeCalled()
+    
     expect(response).toEqual(apiResponse)
-    done()
+
   })
+
+  
 })
--- src/api/getDeposits.js
+++ src/api/getDeposits.js
@@ -3,12 +3,12 @@ const post = require('../lib/dvf/post-authenticated')
 const validateAssertions = require('../lib/validators/validateAssertions')
 
 module.exports = async (dvf, token, nonce, signature) => {
-  const endpoint = '/v1/trading/r/getDeposits'
-
   if (token) {
     const assertionError = await validateAssertions({ dvf, token })
     if (assertionError) return assertionError
   }
+
+  const endpoint = '/v1/trading/r/getDeposits'
 
   const data = {token}
 
