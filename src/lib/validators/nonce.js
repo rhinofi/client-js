73a30d453b1fbc2a4abdaf921aa8562818df5a1c
AliMeer <ali@purple7.com>
2020-02-05 21:36:09 +0000

    reverted nonce to be valid for 3 hours

--- src/lib/validators/nonce.js
+++ src/lib/validators/nonce.js
@@ -6,8 +6,8 @@ module.exports = (dvf, nonce) => {
   if (!nonce || isNaN(nonce)) {
     throw new DVFError('ERR_INVALID_NONCE')
   }
-  
-  if ((Date.now() / 1000) - nonce > 3 * 60 * 60 * 1000) {
+
+  if (Date.now() / 1000 - nonce > dvf.defaultNonceAge) {
     throw new DVFError('NONCE_IS_TOO_OLD')
   }
 }
