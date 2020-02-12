a6863a3b774cfa193ab739f79fc65e91ed3916dd
AliMeer <ali@purple7.com>
2020-02-12 16:24:48 +0000

    updated new token ids

--- src/api/test/fixtures/getConf.js
+++ src/api/test/fixtures/getConf.js
@@ -15,28 +15,32 @@ module.exports = () => {
         decimals: 18,
         quantization: 10000000000,
         minOrderSize: 0.1,
-        starkTokenId: '0x1'
+        starkTokenId:
+          '0xb333e3142fe16b78628f19bb15afddaef437e72d6d7f5c6c20c6801a27fba6'
       },
       USD: {
         decimals: 6,
         quantization: 1,
         minOrderSize: 25,
         settleSpread: -0.026,
-        starkTokenId: '0x2',
+        starkTokenId:
+          '0x180bef8ae3462e919489763b84dc1dc700c45a249dec4d1136814a639f2dd7b',
         tokenAddress: '0x4c5f66596197a86fb30a2435e2ef4ddcb39342c9'
       },
       ZRX: {
         decimals: 18,
         quantization: 10000000000,
         minOrderSize: 40,
-        starkTokenId: '0x3',
+        starkTokenId:
+          '0x3901ee6a6c5ac0f6e284f4273b961b7e9f29d25367d31d90b75820473a202f7',
         tokenAddress: '0xcd077abedd831a3443ffbe24fb76661bbb17eb69'
       },
       BTC: {
         decimals: 18,
         quantization: 10000000000,
         minOrderSize: 0.0001,
-        starkTokenId: '0x4',
+        starkTokenId:
+          '0x21ef21d6b234cd669edd702dd3d1d017be888337010b950ae3679eb4194b4bc',
         tokenAddress: '0x40d8978500bf68324a51533cd6a21e3e59be324a'
       }
     },
@@ -61,7 +65,8 @@ module.exports = () => {
         decimals: 18,
         quantization: 10000000000,
         minOrderSize: 0.1,
-        starkTokenId: '0x1',
+        starkTokenId:
+          '0xb333e3142fe16b78628f19bb15afddaef437e72d6d7f5c6c20c6801a27fba6',
         starkVaultId: 1000001
       },
       USD: {
@@ -69,7 +74,8 @@ module.exports = () => {
         quantization: 1,
         minOrderSize: 25,
         settleSpread: -0.026,
-        starkTokenId: '0x2',
+        starkTokenId:
+          '0x180bef8ae3462e919489763b84dc1dc700c45a249dec4d1136814a639f2dd7b',
         tokenAddress: '0x4c5f66596197a86fb30a2435e2ef4ddcb39342c9',
         starkVaultId: 1000002
       },
@@ -77,7 +83,8 @@ module.exports = () => {
         decimals: 18,
         quantization: 10000000000,
         minOrderSize: 40,
-        starkTokenId: '0x3',
+        starkTokenId:
+          '0x3901ee6a6c5ac0f6e284f4273b961b7e9f29d25367d31d90b75820473a202f7',
         tokenAddress: '0xcd077abedd831a3443ffbe24fb76661bbb17eb69',
         starkVaultId: 1000003
       },
@@ -85,7 +92,8 @@ module.exports = () => {
         decimals: 18,
         quantization: 10000000000,
         minOrderSize: 0.0001,
-        starkTokenId: '0x4',
+        starkTokenId:
+          '0x21ef21d6b234cd669edd702dd3d1d017be888337010b950ae3679eb4194b4bc',
         tokenAddress: '0x40d8978500bf68324a51533cd6a21e3e59be324a',
         starkVaultId: 1000004
       }
