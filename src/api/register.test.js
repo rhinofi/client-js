bd9607f17bc5dc12f2cba1f2c8fef8e6b85713bb
AliMeer <ali@purple7.com>
2020-02-12 17:08:53 +0000

    updated register test cases

--- src/api/register.test.js
+++ src/api/register.test.js
@@ -26,6 +26,8 @@ describe('dvf.register', () => {
       x: fullPublicKey.pub.getX().toString('hex'),
       y: fullPublicKey.pub.getY().toString('hex')
     }
+    const deFiSignature =
+      '0xb5c3802c7cd4a6832c65b35f7011640ab4307f2109451f3db26f2ccf81639e756b109d63dade93ea7f879c735a11b4a0a6671e308a70b106639b15d43f001aac1c'
     nock(dvf.config.api)
       .post('/v1/trading/w/register', body => {
         return (
@@ -38,15 +40,16 @@ describe('dvf.register', () => {
       })
       .reply(200, apiResponse)
 
-    const result = await dvf.register(starkPublicKey)
+    const result = await dvf.register(starkPublicKey, deFiSignature)
     expect(result).toEqual(apiResponse)
   })
 
-  it.only('Register method checks for starkKey', async () => {
+  it('Register method checks for starkKey', async () => {
     const starkPublicKey = ''
+    const deFiSignature = '0xa1b2c3'
 
     try {
-      await dvf.register(starkPublicKey)
+      await dvf.register(starkPublicKey, deFiSignature)
 
       throw new Error('function should throw')
     } catch (error) {
