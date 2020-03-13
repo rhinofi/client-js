1335865a95a6e749cda17aa7e90b9150e2636881
AliMeer <ali@purple7.com>
2020-03-13 11:16:31 +0000

    variable name change sark -> stark

--- src/api/contract/getStarkKey.js
+++ src/api/contract/getStarkKey.js
@@ -5,14 +5,14 @@ module.exports = async dvf => {
   const ethAddress = dvf.get('account')
   const args = [ethAddress]
   try {
-    const sarkKeyDecimal = await dvf.eth.call(
+    const starkKeyDecimal = await dvf.eth.call(
       dvf.contract.abi.StarkEx,
       dvf.config.DVF.starkExContractAddress,
       'getStarkKey',
       args
     )
 
-    return new BN(sarkKeyDecimal).toString(16)
+    return new BN(starkKeyDecimal).toString(16)
   } catch (e) {
     console.log('contract/getStarkKey error is: ', e)
     throw new DVFError('ERR_GETTING_STARK_KEY')
