fd653b0136665631010b927524bb5eafe53f4c20
AliMeer <ali@purple7.com>
2020-03-23 22:38:25 +0000

    making ledger transfer sign method usable for withdrawal as well

--- src/lib/stark/ledger/createSignedTransfer.js
+++ src/lib/stark/ledger/createSignedTransfer.js
@@ -3,30 +3,48 @@ const Eth = require('@ledgerhq/hw-app-eth').default
 const byContractAddress = require('@ledgerhq/hw-app-eth/erc20')
   .byContractAddress
 const DVFError = require('../../dvf/DVFError')
+const BN = require('bignumber.js')
 
 module.exports = async (
-  path, // string a path in BIP 32 format
-  transferTokenAddress, // string?
-  transferQuantization, // BigNumber quantization used for the token to be transferred
+  dvf,
+  path,
+  token,
+  amount,
   sourceVault, // number ID of the source vault
-  destinationVault, // number ID of the destination vault
-  amountTransfer, // BigNumber amount to transfer
-  nonce, // number transaction nonce
-  timestamp // number transaction validity timestamp
+  destinationVault // number ID of the destination vault
 ) => {
-  const transport = await Transport.open()
+  const currency = dvf.token.getTokenInfo(token)
+  // console.log({ currency })
+  const nonce = dvf.util.generateRandomNonce()
+  let transferTokenAddress = currency.tokenAddress
+  const transferQuantization = new BN(currency.quantization)
+  const amountTransfer = new BN(dvf.token.toBaseUnitAmount(token, amount))
+  expireTime =
+    Math.floor(Date.now() / (1000 * 3600)) +
+    parseInt(dvf.config.defaultStarkExpiry)
+
+  const transport = await Transport.create()
   const eth = new Eth(transport)
   const starkKey = (await eth.starkGetPublicKey(path)).toString('hex')
   console.log({ transferTokenAddress })
   if (transferTokenAddress) {
-    const tokenInfo = byContractAddress(transferTokenAddress)
-
+    let tokenInfo
+    tokenInfo = byContractAddress(transferTokenAddress)
+    transferTokenAddress = transferTokenAddress.substr(2)
     if (tokenInfo) {
       await eth.provideERC20TokenInformation(tokenInfo)
     } else {
-      throw new DVFError('LEDGER_TOKENINFO_ERR')
+      console.log('no tokenInfo')
+      // TODO: Remove this for live
+      // console.log('error ', error)
+      // throw new DVFError('LEDGER_TOKENINFO_ERR')
+      tokenInfo = {}
+      tokenInfo['data'] = Buffer.from(
+        `00${transferTokenAddress}0000000000000000`,
+        'hex'
+      )
+      await eth.provideERC20TokenInformation(tokenInfo)
     }
-    transferTokenAddress = transferTokenAddress.substr(2)
   } else {
     transferTokenAddress = null
   }
@@ -41,9 +59,12 @@ module.exports = async (
       destinationVault,
       amountTransfer,
       nonce,
-      timestamp
+      expireTime
     )
   ).toString('hex')
 
-  return { starkKey, starkSignature }
+  transport.close()
+  const starkPublicKey = { x: starkKey }
+  starkTransferData = { starkPublicKey, nonce, expireTime, starkSignature }
+  return starkTransferData
 }
