cd5467bc3635b3499b58b7a29a2d614cf4aab4a4
AliMeer <ali@purple7.com>
2020-03-23 22:39:15 +0000

    Token descriptors for test tokens

--- src/api/ledger/deposit.test.js
+++ src/api/ledger/deposit.test.js
@@ -1,10 +1,10 @@
 const nock = require('nock')
 const instance = require('../test/helpers/instance')
-
 const mockGetConf = require('../test/fixtures/getConf')
-
-const sw = require('starkware_crypto')
-const _ = require('lodash')
+const Transport = require('@ledgerhq/hw-transport-node-hid').default
+const Eth = require('@ledgerhq/hw-app-eth').default
+const byContractAddress = require('@ledgerhq/hw-app-eth/erc20')
+  .byContractAddress
 
 let dvf
 
@@ -14,10 +14,48 @@ describe('dvf.deposit', () => {
     dvf = await instance()
   })
 
-  it.skip(`Deposits ERC20 token to user's vault`, async () => {
+  it('Deposits ETH to users vault', async () => {
     mockGetConf()
     const path = `21323'/0`
-    const amount = 231
+    const token = 'ETH'
+    const amount = 1.117
+    starkPublicKey = {
+      x: '0615a13d3f18d240a1ad98ba9c12ac7b70361a547284b55a35c82f2c2d4515cd'
+    }
+    const apiResponse = {
+      token,
+      amount,
+      starkPublicKey
+    }
+
+    const payloadValidator = jest.fn(body => {
+      expect(body).toMatchObject(apiResponse)
+      expect(typeof body.nonce).toBe('number')
+      expect(body.starkSignature).toMatch(/[\da-f]/i)
+      expect(typeof body.starkVaultId).toBe('number')
+      expect(typeof body.expireTime).toBe('number')
+      expect(body.ethTxHash).toMatch(/[\da-f]/i)
+      return true
+    })
+
+    nock(dvf.config.api)
+      .post('/v1/trading/w/deposit', payloadValidator)
+      .reply(200, apiResponse)
+
+    const starkDeposit = await dvf.stark.ledger.createDepositData(
+      path,
+      token,
+      amount
+    )
+    await dvf.ledger.deposit(token, amount, starkDeposit)
+
+    expect(payloadValidator).toBeCalled()
+  })
+
+  it(`Deposits ERC20 token to user's vault`, async () => {
+    mockGetConf()
+    const path = `21323'/0`
+    const amount = 193
     const token = 'USDT'
     const starkPublicKey = {
       x: '0615a13d3f18d240a1ad98ba9c12ac7b70361a547284b55a35c82f2c2d4515cd'
@@ -43,40 +81,12 @@ describe('dvf.deposit', () => {
       .post('/v1/trading/w/deposit', payloadValidator)
       .reply(200, apiResponse)
 
-    await dvf.ledger.deposit(token, amount, path)
-
-    expect(payloadValidator).toBeCalled()
-  })
-
-  it('Deposits ETH to users vault', async () => {
-    mockGetConf()
-
-    const path = `21323'/0`
-    const token = 'ETH'
-    const amount = 1.117
-    const apiResponse = {
+    const starkDeposit = await dvf.stark.ledger.createDepositData(
+      path,
       token,
-      amount,
-      starkPublicKey: {
-        x: '0615a13d3f18d240a1ad98ba9c12ac7b70361a547284b55a35c82f2c2d4515cd'
-      }
-    }
-
-    const payloadValidator = jest.fn(body => {
-      expect(body).toMatchObject(apiResponse)
-      expect(typeof body.nonce).toBe('number')
-      expect(body.starkSignature).toMatch(/[\da-f]/i)
-      expect(typeof body.starkVaultId).toBe('number')
-      expect(typeof body.expireTime).toBe('number')
-      expect(body.ethTxHash).toMatch(/[\da-f]/i)
-      return true
-    })
-
-    nock(dvf.config.api)
-      .post('/v1/trading/w/deposit', payloadValidator)
-      .reply(200, apiResponse)
-
-    await dvf.ledger.deposit(token, amount, path)
+      amount
+    )
+    await dvf.ledger.deposit(token, amount, starkDeposit)
 
     expect(payloadValidator).toBeCalled()
   })
