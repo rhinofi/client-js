c79eeaa0b9839e6d667707ad1ad5b6b02dc302d7
AliMeer <ali@purple7.com>
2020-03-23 22:40:16 +0000

    adding withdrawal for ledger users

---
+++ src/api/ledger/withdraw.js
@@ -0,0 +1,35 @@
+const { post } = require('request-promise')
+const validateAssertions = require('../../lib/validators/validateAssertions')
+
+module.exports = async (dvf, token, amount, starkWithdrawal) => {
+  validateAssertions(dvf, {
+    token,
+    amount
+  })
+
+  // console.log({ currency })
+  const nonce = starkWithdrawal.nonce
+  const starkVaultId = starkWithdrawal.starkVaultId
+  const expireTime = starkWithdrawal.expireTime
+  const starkPublicKey = starkWithdrawal.starkPublicKey
+  const starkSignature = starkWithdrawal.starkSignature
+
+  // TODO: This could be updated to send starkWithdrawal
+  // However this will require updates to public api
+  // and public api reference documents
+
+  const data = {
+    token,
+    amount,
+    nonce,
+    starkPublicKey,
+    starkSignature,
+    starkVaultId,
+    expireTime
+  }
+  //console.log({ data })
+  const url = dvf.config.api + '/v1/trading/w/withdraw'
+  return post(url, {
+    json: data
+  })
+}
