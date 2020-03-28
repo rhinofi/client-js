75cc1608aa80b8b8973cf949279780c1fdb46bd3
AliMeer <ali@purple7.com>
2020-03-28 21:12:17 +0000

    Ledger Wallet Integration for Submit Order

---
+++ src/lib/stark/ledger/createOrderMetaData.js
@@ -0,0 +1,36 @@
+const validAssertions = require('../../validators/validateAssertions')
+
+module.exports = async (
+  dvf,
+  symbol,
+  amount,
+  price,
+  validFor,
+  feeRate = 0.0025,
+  path
+) => {
+  validAssertions(dvf, { amount, symbol, price })
+
+  const { starkOrder, starkMessage } = dvf.stark.createOrder(
+    symbol,
+    amount,
+    price,
+    validFor,
+    feeRate
+  )
+
+  const {
+    starkPublicKey,
+    starkSignature
+  } = await dvf.stark.ledger.createSignedOrder(path, starkOrder)
+
+  return {
+    starkPublicKey,
+    starkOrder,
+    starkMessage,
+    starkSignature,
+    symbol,
+    amount,
+    price
+  }
+}
