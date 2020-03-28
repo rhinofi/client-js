ce244ffc154a4d7a972edc00c694ffa0548d743f
AliMeer <ali@purple7.com>
2020-03-28 18:13:34 +0000

    update example for submitOrder

--- examples/05.submitOrder.js
+++ examples/05.submitOrder.js
@@ -7,7 +7,6 @@ const Web3 = require('web3')
 const DVF = require('../src/dvf')
 const envVars = require('./helpers/loadFromEnvOrConfig')()
 
-
 const ethPrivKey = envVars.ETH_PRIVATE_KEY
 // NOTE: you can also generate a new key using:`
 // const starkPrivKey = dvf.stark.createPrivateKey()
@@ -22,35 +21,40 @@ const dvfConfig = {
   api: 'https://api.deversifi.dev'
 }
 
-
 ;(async () => {
   const dvf = await DVF(web3, dvfConfig)
 
-  // Submit an order to sell 0.3 Eth for USDT ad 500 USDT per 1 Eth
-  const submitOrderResponse = await dvf.submitOrder(
+  // Submit an order to sell 0.3 Eth for USDT ad 250 USDT per 1 Eth
+
+  // First create data parameters by calling createOrderMetaData
+  // this data will be required for submitting order
+  const orderMetaData = await dvf.stark.createOrderMetaData(
     'ETH:USDT', // symbol
     -0.3, // amount
-    500, // price
-    '', // gid
-    '', // cid
-    '0', // signedOrder
+    250, // price
     0, // validFor
-    'P1', // partnerId
     '', // feeRate
-    '', // dynamicFeeRate
     starkPrivKey
   )
 
-  console.log("submitOrder response ->", submitOrderResponse)
+  // Call submitOrder with the data recieved from createOrderMetaData
+  const submitOrderResponse = await dvf.submitOrder(
+    '1', // gid
+    '1', // cid
+    'P1', // partnerId can be blank
+    '0', // feeRate
+    '0', // dynamicFeeRate
+    orderMetaData
+  )
 
+  console.log('submitOrder response ->', submitOrderResponse)
 })()
-// Stop provider to allow process to exit.
-.then(() => {
-  console.log('Stopping provider...')
-  provider.engine.stop()
-})
-.catch(error => {
-  console.error(error)
-  process.exit(1)
-})
-
+  // Stop provider to allow process to exit.
+  .then(() => {
+    console.log('Stopping provider...')
+    provider.engine.stop()
+  })
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
