8063d69b6cc7ab6ff657f86b853faa64f71c43b6
AliMeer <ali@purple7.com>
2020-04-01 10:07:57 +0100

    separating orderPayload method

--- src/api/submitOrder.js
+++ src/api/submitOrder.js
@@ -1,33 +1,7 @@
 const { post } = require('request-promise')
-const FP = require('lodash/fp')
+
 // TODO: define a schema for data and validate it.
-module.exports = async (dvf, orderData) => {
-  // allow passing in ethAddress (useful for testing).
-  const ethAddress = orderData.ethAddress || dvf.get('account')
-
-  data = {
-    type: 'EXCHANGE LIMIT',
-    protocol: 'stark',
-    feeRate: 0.0025,
-    ...FP.pick(
-      [
-        'amount',
-        'cid',
-        'dynamicFeeRate',
-        'feeRate',
-        'gid',
-        'partnerId',
-        'price',
-        'symbol'
-      ],
-      orderData
-    ),
-    meta: {
-      ethAddress,
-      ...(await dvf.createOrderMetaData(orderData))
-    }
-  }
-  const url = dvf.config.api + '/v1/trading/w/submitOrder'
-
-  return post(url, { json: data })
-}
+module.exports = async (dvf, orderData) =>
+  post(dvf.config.api + '/v1/trading/w/submitOrder', {
+    json: await dvf.createOrderPayload(orderData)
+  })
