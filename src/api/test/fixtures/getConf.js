c67e7a1797b7b1974187801e37c4665bb3f9b3cd
AliMeer <ali@purple7.com>
2020-04-03 09:37:39 +0100

    updated stark contract address

--- src/api/test/fixtures/getConf.js
+++ src/api/test/fixtures/getConf.js
@@ -5,7 +5,7 @@ module.exports = () => {
     DVF: {
       exchangeSymbols: ['tETHUSD', 'tZRXUSD', 'tZRXETH'],
       exchangeAddress: '0xBd25cD867C304F079E696CBE44D958f3d3B683ba',
-      starkExContractAddress: '0xc54910Ce7dfaCB54BdF69f860AaECEe7785C516A',
+      starkExContractAddress: '0x10cE5de6738671FF6cfF136f22f2EEa32837257f',
       tempStarkVaultId: 1,
       defaultFeeRate: 0.0025
     },
@@ -54,7 +54,7 @@ module.exports = () => {
     DVF: {
       exchangeSymbols: ['tETHUSD', 'tZRXUSD', 'tZRXETH'],
       exchangeAddress: '0xBd25cD867C304F079E696CBE44D958f3d3B683ba',
-      starkExContractAddress: '0xc54910Ce7dfaCB54BdF69f860AaECEe7785C516A',
+      starkExContractAddress: '0x10cE5de6738671FF6cfF136f22f2EEa32837257f',
       tempStarkVaultId: 1,
       defaultFeeRate: 0.0025
     },
