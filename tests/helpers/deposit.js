aa41bd8515e7db2717051cf9ef7a483e2e68033b
Thiago Kroger <thiagokroger@gmail.com>
2020-04-08 09:33:53 -0300

    integration tests adding deposit and withdraw (increase deposit time)

--- tests/helpers/deposit.js
+++ tests/helpers/deposit.js
@@ -4,7 +4,7 @@ const deposit = async (dvf, token, amount, starkPrivKey) => {
     try {
         const depositResponse = await dvf.deposit(token, amount, starkPrivKey)
 
-        await P.retry({ times: 20, interval: 30000 }, () => checkBalance(dvf))
+        await P.retry({ times: 20, interval: 120000 }, () => checkBalance(dvf))
 
         return depositResponse
     } catch (err) {
