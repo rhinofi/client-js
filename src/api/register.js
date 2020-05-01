10243f77ddbcd1c53aba5b39f3de1b798f8f014e
AliMeer <ali@purple7.com>
2020-05-02 00:13:26 +0100

    remove pre-register method

--- src/api/register.js
+++ src/api/register.js
@@ -4,33 +4,34 @@ const validateAssertions = require('../lib/validators/validateAssertions')
 module.exports = async (dvf, starkPublicKey) => {
   validateAssertions(dvf, { starkPublicKey })
 
-  const ethAddress = dvf.get('account')
   const starkKey = starkPublicKey.x
-  let url = dvf.config.api + '/v1/trading/w/preRegister'
-  let data = {
-    starkKey,
-    ethAddress
-  }
-
-  const { deFiSignature } = await post(url, { json: data })
-
-  const onchainRegister = await dvf.stark.register(dvf, starkKey, deFiSignature)
-
-  // console.log('onchain register contract call result: ', onchainRegister)
-  if (onchainRegister.error) {
-    return onchainRegister
-  }
 
   const nonce = Date.now() / 1000 + ''
   const signature = await dvf.sign(nonce.toString(16))
 
-  url = dvf.config.api + '/v1/trading/w/register'
+  const url = dvf.config.api + '/v1/trading/w/register'
 
-  data = {
+  const data = {
     starkKey,
     nonce,
     signature
   }
 
-  return post(url, { json: data })
+  const userRegistered = await post(url, { json: data })
+
+  if (userRegistered.isRegistered) {
+    return userRegistered
+  }
+
+  if (userRegistered.deFiSignature) {
+    const onchainRegister = await dvf.stark.register(
+      dvf,
+      starkKey,
+      userRegistered.deFiSignature
+    )
+
+    if (onchainRegister) {
+      return dvf.getUserConfig()
+    }
+  }
 }
