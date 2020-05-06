4433fcb5d8d4a871df9b6b51e274348d7f7ef303
AliMeer <ali@purple7.com>
2020-05-06 19:48:12 +0100

    full and onchain withdrawals added

--- examples/20.ledgerSubmitOrder.js
+++ examples/20.ledgerSubmitOrder.js
@@ -18,7 +18,7 @@ const provider = new HDWalletProvider(ethPrivKey, infuraURL)
 const web3 = new Web3(provider)
 
 const dvfConfig = {
-  // Using dev API.
+  // Using staging API.
   api: 'https://api.stg.deversifi.com'
 }
 
@@ -26,11 +26,27 @@ const dvfConfig = {
 ;(async () => {
   const dvf = await DVF(web3, dvfConfig)
 
-  const token = 'ETH'
+  // Submit an order to sell 0.3 Eth for USDT at 250 USDT per 1 Eth
+  const symbol = 'ETH:USDT'
+  const amount = -0.3
+  const price = 250
+  const validFor = '0'
+  const feeRate = ''
+  const ledgerPath= `44'/60'/0'/0'/0`
 
-  const getVaultIdResponse = await dvf.getVaultId(token)
+  const submitOrderResponse = await dvf.submitOrder({
+    symbol,
+    amount,
+    price,
+    ledgerPath,
+    validFor,           // Optional
+    feeRate,            // Optional
+    gid: '1',           // Optional
+    cid: '1',           // Optional
+    partnerId: 'P1'    // Optional
+  })
 
-  console.log('getVaultId response ->', getVaultIdResponse)
+  console.log('submitOrder response ->', submitOrderResponse)
 
 })()
 // Stop provider to allow process to exit.
