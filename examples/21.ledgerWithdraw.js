cf454b1fa154eae752c489eb228bce557017dc91
AliMeer <ali@purple7.com>
2020-05-09 20:08:29 +0100

    order tweaks and ledger additions

--- examples/21.ledgerWithdraw.js
+++ examples/21.ledgerWithdraw.js
@@ -26,37 +26,23 @@ const dvfConfig = {
 ;(async () => {
   const dvf = await DVF(web3, dvfConfig)
 
+  const path = `44'/60'/0'/0'/0`
+  const token = 'ETH'
+  const amount = 0.10
 
-  const symbol = 'BTC:USDT'
+  const starkWithdrawalData = await dvf.stark.ledger.createWithdrawalData(
+    path,
+    token,
+    amount
+  )
 
-  let orders = await dvf.getOrders(symbol)
+  const withdrawResponse = await dvf.ledger.withdraw(
+    token,
+    amount,
+    starkWithdrawalData
+  )
 
-  if (orders.length == 0) {
-
-    console.log(`no orders for ${symbol}, submitting one`)
-
-    // Submit an order to buy 0.02 BTC at a rate of 7000 USDT for 1 BTC
-    const amount = 0.02
-    const price = 7000
-    const validFor = '0'
-    const feeRate = ''
-
-    const submitOrderResponse = await dvf.submitOrder({
-      symbol,
-      amount,
-      price,
-      starkPrivateKey: starkPrivKey,
-      validFor,           // Optional
-      feeRate,            // Optional
-      gid: '1',           // Optional
-      cid: '1',           // Optional
-      partnerId: 'P1'    // Optional
-    })
-  }
-
-  orders = await dvf.getOrders(symbol)
-
-  console.log("getOrders(symbol) response ->", orders)
+  console.log('withdraw response ->', 'withdrawResponse')
 
 })()
 // Stop provider to allow process to exit.
--- examples/src/submitOrder.js
+++ examples/src/submitOrder.js
@@ -1,7 +1,7 @@
-// Submit an order to sell 0.3 Eth for USDT at 250 USDT per 1 Eth
+// Submit an order to sell 0.3 Eth for 200 USDT per 1 Eth
 const symbol = 'ETH:USDT'
 const amount = -0.3
-const price = 250
+const price = 200
 const validFor = '0'
 const feeRate = ''
 
