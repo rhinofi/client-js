cb49a3c53e1fd7af29ef21a6a51fc473aa65940e
AliMeer <ali@purple7.com>
2020-06-03 09:40:35 +0100

    market buy orders fix

--- src/lib/dvf/createMarketOrderMetaData.js
+++ src/lib/dvf/createMarketOrderMetaData.js
@@ -19,7 +19,7 @@ const starkSignedOrder = async (dvf, starkPrivateKey, starkMessage) => {
 module.exports = async (dvf, orderData) => {
   validateProps(dvf, ['amountToSell', 'symbol', 'tokenToSell', 'worstCasePrice'], orderData)
 
-  const { starkOrder, starkMessage } = await dvf.stark.createOrder(orderData)
+  const { starkOrder, starkMessage } = await dvf.stark.createMarketOrder(orderData)
 
   const { starkPublicKey, starkSignature } = await (orderData.ledgerPath
     ? dvf.stark.ledger.createSignedOrder(orderData.ledgerPath, starkOrder)
