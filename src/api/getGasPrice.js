684004d6bdb1aff6b5133dd421f555f02ed3de38
Adrian Gierakowski <agierakowski@gmail.com>
2020-10-05 19:42:24 +0100

    comment out console.log

--- src/api/getGasPrice.js
+++ src/api/getGasPrice.js
@@ -3,17 +3,18 @@ const { post } = require('request-promise')
 module.exports = async (dvf) => {
   const defaultGasPrice = dvf.config.defaultGasPrice
   const oldGasPrice = { fast: defaultGasPrice * 1.2, average: defaultGasPrice * 0.9, cheap: defaultGasPrice * 0.8 }
-  
+
   const endpoint = '/v1/trading/r/getGasPrice'
   const url = dvf.config.api + endpoint
-  
+
   try {
     const newGasPrice = await post(url, { json: {} })
     dvf.config.defaultGasPrice = newGasPrice.fast || dvf.config.defaultGasPrice
     return newGasPrice ||  oldGasPrice
   }
   catch (e) {
-    console.log('failed to get gas price from dvf pub api')
+    // TODO: user dvf-logger
+    // console.log('failed to get gas price from dvf pub api')
     return oldGasPrice
   }
 }
