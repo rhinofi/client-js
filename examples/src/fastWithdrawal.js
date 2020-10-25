3f6039eeae6c41a18505832efc722239ac923f02
Adrian Gierakowski <agierakowski@gmail.com>
2020-10-25 04:45:43 +0000

    fastWithdrawal: allow setting recipientEthAddress (defaults to users address)

--- examples/src/fastWithdrawal.js
+++ examples/src/fastWithdrawal.js
@@ -1,4 +1,6 @@
 const fastWithdrawalResponse = await dvf.fastWithdrawal(
+  // recipientEthAddress could be added here to send the withdrawal to address
+  // other then users registered address.
   { token: 'ETH', amount: 0.1 },
   starkPrivKey
 )
