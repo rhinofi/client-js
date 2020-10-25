9e35a7cc7174c18c68169671e306d85268032a1c
Adrian Gierakowski <agierakowski@gmail.com>
2020-10-25 03:44:13 +0000

    remove console.log

--- src/lib/util/prepareDepositAmount.js
+++ src/lib/util/prepareDepositAmount.js
@@ -2,6 +2,6 @@ const { prepareAmount, BN } = require('dvf-utils')
 
 module.exports = (dvf, amount, token) => prepareAmount(
   amount,
-  console.log('dvf.token.maxQuantizedDecimalPlaces(token)', dvf.token.maxQuantizedDecimalPlaces(token)) || dvf.token.maxQuantizedDecimalPlaces(token),
+  dvf.token.maxQuantizedDecimalPlaces(token),
   BN.ROUND_FLOOR
 )
