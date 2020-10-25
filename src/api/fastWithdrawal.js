a2ce5a0a799d56422e1d489b9bf2b3c4c574cbdf
Adrian Gierakowski <agierakowski@gmail.com>
2020-10-25 03:44:57 +0000

    fix undefined url in api/fastWithdrawal

--- src/api/fastWithdrawal.js
+++ src/api/fastWithdrawal.js
@@ -1,6 +1,7 @@
 const { post } = require('request-promise')
 
 module.exports = async (dvf, withdrawalData, starkPrivateKey) => {
-  json = await dvf.createFastWithdrawalPayload(withdrawalData, starkPrivateKey)
+  const url = dvf.config.api + '/v1/trading/w/fastWithdrawal'
+  const json = await dvf.createFastWithdrawalPayload(withdrawalData, starkPrivateKey)
   return post(url, { json })
 }
