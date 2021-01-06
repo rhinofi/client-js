c81407bd71e2e35ae74274ba62d9cc0faa01f16d
Will Harborne <will.harborne@gmail.com>
2021-01-06 15:47:40 +0000

    Update src/api/contract/getStarkKey.js
    Co-authored-by: Adrian <agierakowski@gmail.com>

--- src/api/contract/getStarkKey.js
+++ src/api/contract/getStarkKey.js
@@ -2,5 +2,5 @@ const DVFError = require('../../lib/dvf/DVFError')
 const BN = require('bignumber.js')
 
 module.exports = async dvf => {
-  throw new Error('getStarkKey contract method has been removed in StarkExV2}')
+  throw new Error('getStarkKey contract method has been removed in StarkExV2')
 }
