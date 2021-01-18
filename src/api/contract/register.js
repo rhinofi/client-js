53da1c35ab57f7b95330b17ed60496e0439c09fe
Mateja Trifunovski <mtrifunovski@gmail.com>
2021-01-18 15:58:05 +0100

    Handle error thrown

--- src/api/contract/register.js
+++ src/api/contract/register.js
@@ -1,5 +1,3 @@
-const DVFError = require('../../lib/dvf/DVFError')
-
 module.exports = async (dvf, tradingKey, deFiSignature, ethAddress) => {
   ethAddress = ethAddress || dvf.get('account')
 
@@ -7,17 +5,12 @@ module.exports = async (dvf, tradingKey, deFiSignature, ethAddress) => {
 
   const args = [ethAddress, `0x${tradingKey}`, deFiSignature]
 
-  try {
-    await dvf.eth.send(
-      dvf.contract.abi.getStarkEx(),
-      dvf.config.DVF.starkExContractAddress,
-      action,
-      args
-    )
-  } catch (error) {
-    console.log('api/contract/register error is: ', error)
-    throw new DVFError('ERR_STARK_REGISTRATION', {error})
-  }
+  await dvf.eth.send(
+    dvf.contract.abi.getStarkEx(),
+    dvf.config.DVF.starkExContractAddress,
+    action,
+    args
+  )
 
   return true
 }
