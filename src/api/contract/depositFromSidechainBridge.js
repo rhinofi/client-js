c9bd2d7ce839fc0081f2459011bdae4900560a3b
Neozaru <neozaru@mailoo.org>
2021-09-24 11:17:51 +0200

    fix: VEN-210 fix bridged native deposit address

--- src/api/contract/depositFromSidechainBridge.js
+++ src/api/contract/depositFromSidechainBridge.js
@@ -1,6 +1,6 @@
 
 module.exports = (dvf, { bridgeContractAddress, tokenAddress, baseUnitAmount }, options) => {
-  const [action, args, value] = tokenAddress === '0x0000000000000000000000000000000000000000'
+  const [action, args, value] = tokenAddress === undefined // Native token
     // For native token (ie: MATIC on Polygon), use specifoc method and pass amount as value
     ? ['depositNative', [], baseUnitAmount]
     // For other tokens, use amount as argument (in baseUnits, unlike starkEx deposits)
