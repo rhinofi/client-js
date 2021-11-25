d696274ca04bd9a4a0eaefbd230086a04c40c224
Arman Yaraee <arman.yaraee@deversify.com>
2021-11-25 13:51:15 +0000

    JUP-168 Removed unused `transferAndWithdrawUsingVaultId`

--- src/api/transferAndWithdrawUsingVaultId.js
+++
@@ -1,9 +0,0 @@
-const { post } = require('request-promise')
-
-module.exports = async (dvf, transferData) => {
-  const url = dvf.config.api + '/v2/trading/w/transferAndWithdraw'
-
-  const json = await dvf.createTransferAndWithdrawPayload(transferData)
-
-  return post(url, { json })
-}
