ce4d961c6b800057848eb8a1b73693d6342b707b
Arman Yaraee <arman.yaraee@deversify.com>
2021-11-26 11:18:19 +0000

    JUP-168 Add getWithdrawalBalanceEthAddress

---
+++ src/api/contract/getWithdrawalBalanceEthAddress.js
@@ -0,0 +1,30 @@
+const DVFError = require('../../lib/dvf/DVFError')
+const BN = require('bignumber.js')
+
+module.exports = async (dvf, token, address) => {
+  address = address || dvf.get('account')
+
+  if (!address) {
+    throw new Error('getWithdrawalBalanceEthAddress: address is required')
+  }
+
+  const starkTokenId = dvf.config.tokenRegistry[token].starkTokenId
+
+  if (!starkTokenId) {
+    throw new Error(`getWithdrawalBalanceEthAddress: no starkTokenId for token: ${token}`)
+  }
+
+  const args = [tradingKey, starkTokenId]
+
+  try {
+    return (withdrawalBalance = await dvf.eth.call(
+      dvf.contract.abi.getStarkEx(),
+      dvf.config.DVF.starkExContractAddress,
+      'getWithdrawalBalance',
+      args
+    ))
+  } catch (e) {
+    console.log('contract/getWithdrawalBalance error is: ', e)
+    throw new DVFError('ERR_GETTING_AVAILABLE_WITHDRAWAL')
+  }
+}
--- src/lib/dvf/bindApi.js
+++ src/lib/dvf/bindApi.js
@@ -69,6 +69,9 @@ module.exports = () => {
     getWithdrawalBalance: compose(
       require('../../api/contract/getWithdrawalBalance')
     ),
+    getWithdrawalBalanceEthAddress: compose(
+      require('../../api/contract/getWithdrawalBalanceEthAddress')
+    ),
     getAllWithdrawalBalances: compose(
       require('../../api/contract/getAllWithdrawalBalances')
     ),
