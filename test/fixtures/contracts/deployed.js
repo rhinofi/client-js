3075c29a9fea5e04187c06b0c28d5b9ad0950ff4
Mateja Trifunovski <mtrifunovski@gmail.com>
2021-11-30 13:25:58 +0100

    Fix

--- test/fixtures/contracts/deployed.js
+++
@@ -1,9 +0,0 @@
-/*
- * Leverages require.js loading cache in order to share an object
- * among multiple files on the same process.
-
- * NOTE:
- *  - this is ugly
- *  - quick hack to make tests run
- */
-module.exports = {}
