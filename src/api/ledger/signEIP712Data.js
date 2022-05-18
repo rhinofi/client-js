64650de7bc588a124d282b49199a20efcd25abb8
Neozaru <neozaru@mailoo.org>
2022-05-18 11:32:39 -0700

    feat: ledger signEIP712 close finally

--- src/api/ledger/signEIP712Data.js
+++ src/api/ledger/signEIP712Data.js
@@ -6,25 +6,23 @@ const { getStructHash } = require('eip-712')
 module.exports = async (dvf, jsonData, ledgerPath) => {
   const Transport = selectTransport(dvf.isBrowser)
   const createdTransport = await Transport.create()
-  const ledgerEth = new Eth(createdTransport)
-
-  const domainStructHash = getStructHash(jsonData, 'EIP712Domain', jsonData.domain)
-  const domainStructString = Buffer.from(domainStructHash).toString('hex')
-
-  const messageStructHash = getStructHash(jsonData, jsonData.primaryType, jsonData.message)
-  const messageStructString = Buffer.from(messageStructHash).toString('hex')
-
-  return ledgerEth.signEIP712HashedMessage(
-    ledgerPath,
-    domainStructString,
-    messageStructString
-  ).then(signature => {
+  try {
+    const ledgerEth = new Eth(createdTransport)
+    const domainStructHash = getStructHash(jsonData, 'EIP712Domain', jsonData.domain)
+    const domainStructString = Buffer.from(domainStructHash).toString('hex')
+    const messageStructHash = getStructHash(jsonData, jsonData.primaryType, jsonData.message)
+    const messageStructString = Buffer.from(messageStructHash).toString('hex')
+    const signature = await ledgerEth.signEIP712HashedMessage(
+      ledgerPath,
+      domainStructString,
+      messageStructString
+    )
     return {
       r: Buffer.from(signature.r, 'hex'),
       s: Buffer.from(signature.s, 'hex'),
       v: signature.v
     }
-  }).finally(() => {
+  } finally {
     createdTransport.close()
-  })
+  }
 }
