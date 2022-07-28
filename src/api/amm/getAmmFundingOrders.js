ba254176648c61c7dec9e7be882ca11769240872
Nicolas BERNARD <nicolas.bernard@deversifi.com>
2022-07-28 21:54:47 +0200

    update amm funding orders endpoint

--- src/api/amm/getAmmFundingOrders.js
+++ src/api/amm/getAmmFundingOrders.js
@@ -1,18 +1,47 @@
 const { Joi } = require('dvf-utils')
+const getAuthenticated = require('../../lib/dvf/get-authenticated')
 
-const get = require('../../lib/dvf/get-generic')
 const validateWithJoi = require('../../lib/validators/validateWithJoi')
 
-const schema = Joi.object({
-  ammFundingOrderId: Joi.string()
-})
+const schema = Joi.alternatives().try(
+  Joi.object({
+    ammFundingOrderId: Joi.string()
+  }),
+  Joi.object({
+    sortDirection: Joi.string()
+      .valid('DESC', 'ASC')
+      .optional(),
+    startDate: Joi.date().optional(),
+    endDate: Joi.date()
+      .greater(Joi.ref('startDate'))
+      .optional(),
+    skip: Joi.number()
+      .integer()
+      .min(0)
+      .optional(),
+    limit: Joi.number()
+      .integer()
+      .min(1)
+      .max(100)
+      .optional(),
+    pool: Joi.string().optional()
+  })
+)
 
 const validateData = validateWithJoi(schema)('INVALID_METHOD_ARGUMENT')({
   context: 'getAmmFundingOrders'
 })
 
-module.exports = (dvf, data) => {
-  const { ammFundingOrderId } = validateData(data)
-  const endpoint = `/v1/trading/amm/fundingOrders/${ammFundingOrderId}`
-  return get(dvf, endpoint)
+module.exports = (dvf, nonce, signature, data) => {
+  const validatedData = validateData(data)
+  const endpoint = `/v1/trading/amm/fundingOrders`
+  if (validatedData.ammFundingOrderId) {
+    return getAuthenticated(
+      dvf,
+      `${endpoint}/${validatedData.ammFundingOrderId}`,
+      nonce,
+      signature
+    )
+  }
+  return getAuthenticated(dvf, endpoint, nonce, signature, validatedData)
 }
