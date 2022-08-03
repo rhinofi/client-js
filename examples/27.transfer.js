c420f31bffff53b32db576882d4014b0f969fe3b
Arman Yaraee <arman.yaraee@deversifi.com>
2022-08-03 16:25:54 +0100

    [IMX] Transfer fetching example

--- examples/27.transfer.js
+++ examples/27.transfer.js
@@ -32,13 +32,58 @@ const dvfConfig = {
 ;(async () => {
   const dvf = await DVF(web3, dvfConfig)
 
-  const transferResponse = await dvf.transfer({
-    recipientEthAddress: '0x5317c63f870e8D2f85f0dE3c2666D1414f5a728c',
-    token: 'USDT',
-    amount: 1
-  })
+  if (process.env.DO_TRANSFER) {
+    // Send a transfer to another address
+    const transferResponse = await dvf.transfer({
+      recipientEthAddress: '0x5472cf4f1be2aa6ad27c6f93101f7899ccadbaf7',
+      token: 'DVF',
+      // Any string, can place your ID here
+      memo: 'some_id_2',
+      amount: 10
+    })
+  }
 
-  logExampleResult(transferResponse)
+  const { nonce, signature } = await dvf.sign.nonceSignature()
+
+  // Paginated endpoint that returns all transfers involving the
+  // authenticated user. You must fetch all, then filter through them
+  const transfers = await dvf.getAuthenticated(
+    '/v1/trading/transfers',
+    nonce,
+    signature,
+    {
+      // Pagination parameters here
+      // Refer to swagger docs for a list of supported
+      // options: https://api.deversifi.dev/v1/trading/docs#/Wallet/getV1TradingTransfers
+    },
+    {}
+  )
+
+  logExampleResult(transfers)
+
+  // Find a transfer with memo = 'some_id_2'
+  const getTransferWithMemo = async (memo, current = 0) => {
+    const limit = 1
+    const { items, pagination } = await dvf.getAuthenticated(
+      '/v1/trading/transfers',
+      nonce,
+      signature,
+      {
+        limit,
+        skip: current
+      },
+      {}
+    )
+
+    const [result] = items.filter(t => t.memo === memo)
+    if (result) return result
+
+    if (pagination.totalItems <= current + limit) return
+
+    return getTransferWithMemo(memo, current + limit)
+  }
+
+  logExampleResult(await getTransferWithMemo('some_id_2'))
 })()
   .catch(error => {
     console.error(error)
