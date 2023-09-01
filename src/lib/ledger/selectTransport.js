0d2fc30c25da6ace473bcee774fb8311783d8026
Dean Selvey <deanselvey@gmail.com>
2023-09-01 10:01:24 +0100

    removed reference to ledger node-hid package

--- src/lib/ledger/selectTransport.js
+++ src/lib/ledger/selectTransport.js
@@ -1,9 +1,4 @@
-module.exports = isBrowser => {
-  if (isBrowser) {
-    const Transport = require('@ledgerhq/hw-transport-webhid').default
-    return Transport
-  } else {
-    const Transport = require('@ledgerhq/hw-transport-node-hid').default
-    return Transport
-  }
+module.exports = () => {
+  const Transport = require('@ledgerhq/hw-transport-webhid').default
+  return Transport
 }
